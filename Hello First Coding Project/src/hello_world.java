d4ea6ceb74b3e023e76359d33cea987f2c08ebff
APCS3rd <APCS3rd@MHS-Math2-26>
2018-08-20 10:44:43 -0700

    first commit to github

---
+++ Hello First Coding Project/src/hello_world.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <your name>"); //Display the string
+    }
+}
+
+
