77874b6c29382ed5693393be3dff5870ad0a4308
Carolyn <Carolyn@192.10.1.103>
2018-09-05 21:20:57 -0700

    capitalized class name

--- Hello First Coding Project/src/Hello_World.java
+++ Hello First Coding Project/src/Hello_World.java
@@ -3,9 +3,9 @@
  * Write a description of class Hello_world here.
  * 
  * @author (Carolyn Yee) 
- * @version (8/25/2018)
+ * @version (9/05/2018, version 3...)
  */
-public class hello_world
+public class Hello_World
 {
     public static void main(String[] args)
     {
