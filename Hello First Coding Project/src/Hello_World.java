70da6b3ee2c2624fa29d2facd475e8851a396b55
APCS3rd <APCS3rd@MHS-Math2-26>
2018-10-30 11:26:04 -0700

    updating a ton of stuff github problems suck. created split.java

---
+++ Hello First Coding Project/src/Hello_World.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (Carolyn Yee) 
+ * @version (9/05/2018, version 3...)
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Carolyn Yee"); //Display the string
+    }
+}
+
+
